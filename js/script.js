5dc82a418247c9d20cbeb58f3348e64b2b57a998
rifkiprnmaa <rifkiprn11@gmail.com>
2024-06-20 06:27:14 +0700

    init configuration

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ function konversi(){
     var number = document.getElementById('number')
     console.log(number)
 }
+
+function konversi(){
+    var celsius = document.getElementById('number').value
+    var fahrenheit = (celsius * 9 / 5) + 32;
+    document.getElementById('fahrenheit').value = fahrenheit
+}
+
+    function resetValue(){
+    document.getElementById('number').value = '';
+}
+
+function reverse(){
+    document.getElementById('celciusText').innerHTML = 'fahrenheit'
+    
+}
